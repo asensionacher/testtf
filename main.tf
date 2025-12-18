7dbaa7e2caa91dd53f6e89763cf8d79da690fc97
Sergi Asensio <seasna@proton.me>
2025-12-18 15:26:48 +0100

    Add prefix variable to main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ type = string
 default = "default"
 }
 
+variable "prefix" {
+  type        = string
+  default     = "default"
+}
+
 module "example" {
   source  = "registry.local:9080/privateprivate/naming/azurerm"
   version = "0.4.2"
