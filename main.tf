18a629c02edcd936fae3b52c97307c0b9191f777
Sergi Asensio <seasna@proton.me>
2025-12-18 15:25:59 +0100

    Add suffix variable to main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+variable "suffix" {
+type = string
+default = "default"
+}
+
 module "example" {
   source  = "registry.local:9080/privateprivate/naming/azurerm"
   version = "0.4.2"
+  suffix = [ var.suffix ]
 }
 
 output "test" {
